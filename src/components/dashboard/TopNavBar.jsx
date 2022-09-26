904c72b0f439df4529c70603d18c56201a9f5cd2
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-09-26 20:22:18 +0000

    Make the top navbar sticky

--- src/components/dashboard/TopNavBar.jsx
+++ src/components/dashboard/TopNavBar.jsx
@@ -3,7 +3,16 @@ import LabelLogo from '../LabelLogo';
 
 const TopNavBar = () => {
   return (
-    <Flex py={5} px={5} justify="space-between" boxShadow="lg">
+    <Flex
+      pos="sticky"
+      top={0}
+      right={0}
+      py={5}
+      px={5}
+      justify="space-between"
+      boxShadow="lg"
+      bg={'white'}
+    >
       <LabelLogo />
 
       <Flex>
@@ -12,6 +21,7 @@ const TopNavBar = () => {
           Joshua Asuman
         </Text>{' '}
         {/* TODO: add lecturer's name here */}
+        {/* FIXME: The buttons pass over me */}
       </Flex>
     </Flex>
   );
