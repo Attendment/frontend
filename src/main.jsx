6a8a87674221e83620b1c54202a6724d9a6e27fb
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-09-26 02:35:14 +0000

    Configure the chakra ui and the react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './pages/Home';
+import ErrorPage from './error-page';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+import { extendTheme, ChakraProvider } from '@chakra-ui/react';
+
+// import css
+// import '@fontsource/raleway/400.css';
+import '@fontsource/open-sans/700.css';
+import '@fontsource/quicksand/400.css';
+import '@fontsource/quicksand/500.css';
+import '@fontsource/quicksand/600.css';
+import '@fontsource/quicksand/700.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Open Sans', sans-serif`,
+    body: `'QuickSand', sans-serif`,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>
-)
+);
